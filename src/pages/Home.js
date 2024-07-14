b734ba468d6c57f87bce8fa8e66de95c363c71c6
Bihary Roland <rolandbihary@gmail.com>
2024-07-14 23:22:44 +0200

    Home page greeting

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,14 @@
 function Home() {
-    return <>bla</>;
+return <div className="w-screen flex justify-center">
+        <div className="max-w-7xl text-center">
+            <h1 className="text-3xl">Welcome to <b>Tarot Guide</b> App</h1>
+            <div className="mt-4 p-2">
+                <p>This project is meant to be an encyclopedia for tarot decks, cards and the meanings behind them.</p>
+                <p>The main puprose behind it is to gather all the important and interesting information about the Tarot.</p>
+                <p>Hope you'll find it useful.</p>
+            </div>
+        </div>
+    </div>;
 }
 
 export default Home
