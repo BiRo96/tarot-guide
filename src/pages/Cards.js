47916d55b0eb4c118fdd51221215c923fdc5f0a6
Bihary Roland <rolandbihary@gmail.com>
2024-07-15 23:47:36 +0200

    the listing of cards is much more transparent

--- src/pages/Cards.js
+++ src/pages/Cards.js
@@ -18,21 +18,23 @@ function Cards() {
 
     
     return (
-        <div className="grid grid-cols-8">
-            {cards ? (
-            <>
-                {cards["cards"].map(item => (
-                    <Link to={"/" + appMainURL + "/card/" + item.name_short}>
-                    <div className="p-2 text-center flex flex-col" key={item.name_short}>
-                        <img src={'/decks/' + deck + '/' + imageFinder(item.eng_name)} className="my-5 pointer-events-none" alt="card" />
-                        <p className="text-xl">{item.name}</p>
-                    </div>
-                    </Link>
-                ))}
-            </>
-            ) : (
-            <p>Loading...</p>
-            )}
+        <div className="w-screen justify-center flex">
+            <div className="grid grid-cols-8 max-w-7xl">
+                {cards ? (
+                <>
+                    {cards["cards"].map(item => (
+                        <Link to={"/" + appMainURL + "/card/" + item.name_short}>
+                        <div className="p-2 text-center flex flex-col" key={item.name_short}>
+                            <img src={'/decks/' + deck + '/' + imageFinder(item.eng_name)} className="my-5 pointer-events-none" alt="card" />
+                            <p className="text-xl">{item.name}</p>
+                        </div>
+                        </Link>
+                    ))}
+                </>
+                ) : (
+                <p>Loading...</p>
+                )}
+            </div>
         </div>
     )
 }
