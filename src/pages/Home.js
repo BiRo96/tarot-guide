0ed6a965cad3245b2eab3e2695a7db94b29a1da1
Bihary Roland <rolandbihary@gmail.com>
2024-07-20 17:48:27 +0200

    higher resolution rider deck from wikipedia

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,24 +2,24 @@ import { Link } from "react-router-dom";
 import { __ } from "../components/LanguageHandler";
 
 function Home() {
-return <div className="w-screen flex justify-center">
+return <div className="flex justify-center w-screen">
         <div className="max-w-7xl text-center">
             <h1 className="text-3xl">{__("Welcome to")} <b>{__("Tarot Guide App")}</b></h1>
-            <div className="mt-4 p-2">
+            <div className="p-2 mt-4">
                 <p>{__("This project is meant to be an encyclopedia for tarot decks, cards and the meanings behind them.")}</p>
                 <p>{__("The main puprose behind it is to gather all the important and interesting information about the Tarot.")}</p>
                 <p>{__("Hope you'll find it useful.")}</p>
             </div>
-            <div className="mt-4 p-2">
+            <div className="p-2 mt-4">
                 <p>{__("Made with")} <b>React</b>, <b>Tailwind</b> {__("and a lot of interest")}</p>
                 <p>{__("by")} <Link to="https://github.com/BiRo96" className="text-blue-300"><b>BiRo</b></Link></p>
             </div>
-            <div className="mt-4 p-2">
+            <div className="p-2 mt-4">
                 <h3 className="text-2xl font-bold">{__("Want to contribute?")}</h3>
                 <p>{__("Check out the project's")} <Link to="https://github.com/BiRo96/tarot-guide" className="text-blue-300"><b>{__("GitHub repository")}</b></Link></p>
                 <p>{__("or feel free to")} <Link to="https://github.com/BiRo96/tarot-guide/issues" className="text-blue-300"><b>{__("open an issue")}</b></Link>.</p>
             </div>
-            <div className="mt-4 p-2">
+            <div className="p-2 mt-4">
                 <h3 className="text-2xl font-bold">{__("Future plans")}</h3>
                 <p>{__("In my plans, there is a huge place for translations.")}</p>
                 <p>{__("I'd like to make this project multilingual to support as much different languages as I can.")}</p>
@@ -27,21 +27,21 @@ return <div className="w-screen flex justify-center">
                 <p className="mt-4">{__("And of course I'm planning to figure out what kind of design I should use for this project.")}</p>
                 <p>{__("It is only the beginning, so it may change drasticly over time. :)")}</p>
             </div>
-            <div className="mt-4 p-2">
+            <div className="p-2 mt-4">
                 <h3 className="text-2xl font-bold">{__("Sources")}</h3>
                 <p>{__("Since I'm not an expert, I can't help you understand the Tarot cards by myself.")}</p>
                 <p>{__("But I try to gather the most reliable sources the create an offline over-all guidebook app.")}</p>
                 <p className="mt-4 font-bold">{__("My main sources")}:</p>
                 <ul className="border-2 border-black">
-                    <li className="border-b border-black py-3 bg-slate-400">
-                        <div className="bg-slate-500"><Link to="https://www.dailytarotdraw.com" className="text-blue-300">Daily Tarot Draw</Link></div>
-                        <div className="bg-slate-800 text-white">({__("I took the original Rider-Waite tarot deck images from here")})</div>
+                    <li className="py-3 border-b border-black bg-slate-400">
+                        <div className="bg-slate-500"><Link to="https://en.wikipedia.org/wiki/Rider%E2%80%93Waite_Tarot" target="_blank" className="text-blue-300">Wikipedia</Link></div>
+                        <div className="text-white bg-slate-800">({__("I took the original Rider-Waite tarot deck images from here")})</div>
                     </li>
-                    <li className="border-b border-black py-3 bg-slate-400">
-                        <div className="bg-slate-500"><Link to="https://chatgpt.com/" className="text-blue-300">ChatGPT 4.0 / 3.5</Link></div>
-                        <div className="bg-slate-800 text-white">({__("It made the card datas and meanings summarized with keywords")})</div>
+                    <li className="py-3 border-b border-black bg-slate-400">
+                        <div className="bg-slate-500"><Link to="https://chatgpt.com/" target="_blank" className="text-blue-300">ChatGPT 4.0 / 3.5</Link></div>
+                        <div className="text-white bg-slate-800">({__("It made the card datas and meanings summarized with keywords")})</div>
                     </li>
-                    <li className="border-b border-black py-3 bg-slate-400">
+                    <li className="py-3 border-b border-black bg-slate-400">
                         <div className="bg-slate-500">{__("Books about the Tarot")}</div>
                     </li>
                     
