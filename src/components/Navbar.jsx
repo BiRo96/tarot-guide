72d1ebba8ade15a23e4d77f074413cb17e37dd56
Bihary Roland <rolandbihary@gmail.com>
2024-07-21 14:27:22 +0200

    navbar elements overflew the bar container.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,29 +20,29 @@ function Navbar() {
         <>
             <nav>
                 <div className='p-4'>
-                    <button onClick={() => setIsOpen(!isOpen)} className="hover:text-slate-400 focus:text-slate-400 focus:outline-none flex">
+                    <button onClick={() => setIsOpen(!isOpen)} className="flex hover:text-slate-400 focus:text-slate-400 focus:outline-none">
                         <GiHamburgerMenu className="mr-2" size={24} /> {__("Menu")}
                     </button>
                 </div>
 
-                <div className="fixed top-0 left-0 bg-slate-800 text-white h-full w-1/4 p-4 transform transition-transform duration-300 ease-in-out"
+                <div className="overflow-hidden fixed top-0 left-0 p-4 w-1/4 h-full text-white transition-transform duration-300 ease-in-out transform bg-slate-800"
                     style={{ transform: isOpen ? 'translateX(0)' : 'translateX(-100%)' }}>
 
-                    <button className='hover:text-slate-400 focus:text-slate-400 focus:outline-none flex pb-4' onClick={() => setIsOpen(false)}><GiHamburgerMenu className="mr-2" size={24} /> {__("Close")}</button>
+                    <button className='flex pb-4 hover:text-slate-400 focus:text-slate-400 focus:outline-none' onClick={() => setIsOpen(false)}><GiHamburgerMenu className="mr-2" size={24} /> {__("Close")}</button>
 
                     <div className="flex items-center mb-4">
-                        <label htmlFor="language" className="text-lg font-bold mr-2">{__("Language")}:</label>
-                        <select id="language" className="bg-slate-700 text-white rounded-md px-2 py-1" value={getLang()} onChange={(e) => setLang(e.target.value)}>
+                        <label htmlFor="language" className="mr-2 text-lg font-bold">{__("Language")}:</label>
+                        <select id="language" className="px-2 py-1 text-white rounded-md bg-slate-700" value={getLang()} onChange={(e) => setLang(e.target.value)}>
                             <option value="eng">{__("English")}</option>
                             <option value="hun">{__("Hungarian")}</option>
                         </select>
                     </div>
 
-                    <h2 className="text-lg font-bold mb-4">{__("Pages")}</h2>
+                    <h2 className="mb-4 text-lg font-bold">{__("Pages")}</h2>
                     <ul>
                         {menuItems.map((item) => (
                             <Link to={"/" + appMainURL + "/" + item.url}>
-                                <li key={"/" + appMainURL + "/" + item.name} className="p-2 cursor-pointer hover:bg-slate-700 flex">
+                                <li key={"/" + appMainURL + "/" + item.name} className="flex p-2 cursor-pointer hover:bg-slate-700">
                                     {item.icon}
                                     {item.name}
                                 </li>
